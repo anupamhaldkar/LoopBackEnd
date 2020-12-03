3a22b6db678889205e65c118be6bcf7451e23f00
Anupam Haldkar <ahdev2020@outlook.com>
2020-12-03 18:21:47 +0530

    loopback Creating user

--- server/server.js
+++ server/server.js
@@ -34,6 +34,20 @@ boot(app, __dirname, function(err) {
 });
 
 console.log(Object.keys(app.models));
-app.models.User.observe('after save',  (ctx) => {
- console.log(ctx);
+app.models.User.afterRemote('create',  (ctx, user, next) => {
+ console.log("New user is",user);
+ app.models.Profile.create({
+   first_name: user.username,
+    created_at: new Date(),
+    userId: user.id,
+ },(err,result) => {
+   if(!err && result){
+     console.log("Created new profile", result);
+   }
+   else{
+     console.log("There is an error",err);
+   }
+   next();
+  });
+
 });

fd7a9ff49f3f6e6b646fcd156c9136642563f9bc
Anupam Haldkar <ahdev2020@outlook.com>
2020-12-11 15:33:10 +0530

    Creating Data Functions🧐

--- server/test.js
+++ server/test.js
@@ -1,4 +1,6 @@
 var models = require('./server.js').models;
+//model.profile.upsert({name:'ASH'},(err,profile))
+//or we can use create() method
 models.Profile.findOrCreate({name:'AH'},(err,profile) => {
   console.log("data?",err,profile);
 })

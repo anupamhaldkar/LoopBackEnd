d053e25f19f0c15dbb032ec714a3dd77670f9522
Anupam Haldkar <ahdev2020@outlook.com>
2020-12-12 15:51:57 +0530

    Updating Data 📅

--- server/test.js
+++ server/test.js
@@ -2,5 +2,13 @@ var models = require('./server.js').models;
 //model.profile.upsert({name:'ASH'},(err,profile))
 //or we can use create() method
 models.Profile.findOrCreate({name:'AH'},(err,profile) => {
-  console.log("data?",err,profile);
+   if(err)
+    console.log("There was an error",err);
+    else if(profile){
+        profile.updateAttributes({
+            email:"a@ah.com"
+        }, (updateError,updated)=>{
+            console.log("saved?",updateError,updated);
+        });
+    }
 })

78d0f0ee4ec7eb4ffb693ccb2a4684f70c261aa7
Anupam Haldkar <ahdev2020@outlook.com>
2020-12-15 16:12:14 +0530

    Name email Insertion using Map

--- server/test.js
+++ server/test.js
@@ -1,15 +1,23 @@
 var models = require('./server.js').models;
-//model.profile.upsert({name:'ASH'},(err,profile))
-//or we can use create() method
-models.Profile.findOrCreate({name:'AH'},(err,profile) => {
-   if(err)
-    console.log("There was an error",err);
-    else if(profile){
-        profile.email='a@ah.com';
-        profile.save((ue,updated) => {
-            console.log("Save?",updated);
-        });
-        
-        
-    }
+
+var toSave =   [
+    {name:'Anupam', email:'a12@h.com'},
+    {name:'Anupam1', email:'a13@h.com'},
+    {name:'Anupam2', email:'a11@h.com'},
+    {name:'Anupam3', email:'a9@h.com'},
+    {name:'Anupamr', email:'a8@h.com'},
+    {name:'Anupam4', email:'a7@h.com'},
+    {name:'Anupam5', email:'a6@h.com'},
+    {name:'Anupam6', email:'a4@h.com'},
+    {name:'Anupam87', email:'a5@h.com'},
+    {name:'Anupam8', email:'a2@h.com'},
+    {name:'Anupam9', email:'a3@h.com'},
+    {name:'Anupam12', email:'a1@h.com'},
+    {name:'Anupa1m', email:'a32@h.com'},
+];
+
+toSave.map( obj => {
+    models.Profile.create(obj, (err,created)=>{
+        console.log("Created?",created);
+    })
 })

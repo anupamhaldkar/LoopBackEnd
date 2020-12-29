6f164b2ab142ea034844fe9987eca47f1144a438
anupamhaldkar <ahdev2020@outlook.com>
2020-12-29 11:58:51 +0530

    ACL and role established

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@
 
 'use strict';
 
+const { Principal } = require('loopback');
 const loopback = require('loopback');
 const boot = require('loopback-boot');
 
@@ -69,6 +70,21 @@ app.models.user.afterRemote('create',  (ctx, user, next) => {
 app.models.Role.find({where:{name:'admin'}},(err, role)=>{
   if(!err&&role){
     console.log('No error, role is',role);
-    
+    if(role.length===0){
+      app.models.Role.create({name:'admin'},(err2,result)=>{
+        if(!err2&&result){
+          app.models.user.findOne((usererr,user)=>{
+            if(!usererr&&user){
+              result.principals.create({
+            principalType:app.models.RoleMapping.USER,
+            principalId:user.id,
+          },(err3,Principal)=>{
+              console.log('Created principal',err3, principal);
+          });
+        }
+      });
+    }
+      });
+    }
   }
-})
+});

c59e5deb4aee91c779aa4abd25a69435df04f1f9
anupamhaldkar <ahdev2020@outlook.com>
2021-01-06 18:46:06 +0530

    Adding Login Button

--- common/models/post-image.js
+++ common/models/post-image.js
@@ -1,5 +1,21 @@
 'use strict';
-
+const sharp = require('sharp');
 module.exports = function(Postimage) {
 
+    Postimage.remoteMethod(
+        'upload',
+        {
+            description: 'uploads a file',
+            accepts: [
+                {arg:'ctx',type:'object', http:{source: 'context'}},
+                {arg:'options', type:'object', http:{source:'query'}},
+                {arg:'access_token', type:'string'},
+                {arg: 'post_ID', type:'string'},
+        ],
+        returns:{
+            arg: 'fileObject', type:'object', root:true,
+        },
+        http:{verb: 'post'},
+        }
+    )
 };

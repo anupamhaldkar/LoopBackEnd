611e81bb1111df4714739e6829597fa0dd13cbd5
anupamhaldkar <ahdev2020@outlook.com>
2021-01-09 19:12:36 +0530

    upload image api call

--- common/models/post-image.js
+++ common/models/post-image.js
@@ -2,14 +2,41 @@
 const sharp = require('sharp');
 const fs = require('fs');
 const CONTAINER_URL = '/api/containers/';
-module.exports = function(Postimage) {
-    PostImage.upload = function(ctx, options, access_token, post_id, cb){
+module.exports = function(PostImage) {
+    PostImage.upload = function(ctx, options, access_token, post_id,user_id ,cb){
         if(!options) options = {};
         ctx.req.params.container = 'postImages';
         if(!cs.existsSync('./server/storage/'+ ctx.req.params.container)){
             fs.mkdirSync('./server/storage/'+ ctx.req.params.container);
         }
-        PostImage.app.models.ImageFile.upload();
+        PostImage.app.models.ImageFile.upload(ctx.req, ctx.result, options,(err,file)=>{
+            if(err){
+                cb(err);
+            }
+            else {
+                var fileInfo = file.files.file[0];
+                sharp('./server/storage'+ctx.req.params.container+'/'+fileInfo.name)
+                .resize(100)
+                .toFile('./server/storage'+ctx.req.params.container+'/100-'+fileInfo.name, (err)=>{
+                    if(!err){
+                        PostImage.create({
+                            url:CONTAINER_URL+fileInfo.container+'/download'+fileInfo.name,
+                            thumbnail:CONTAINER_URL+fileInfo.container + '/download/100-'+fileInfo.name,
+                            created_at:new Date(),
+                            post_id:post_id,
+                            user_id:user_id,
+                        },(err2,image)=>{
+                            if(err2){
+                                cb(err2);
+                            }
+                            else{
+                                cb(null,image);
+                            }
+                        });
+                    }
+                })
+            }
+        });
     }
     PostImage.remoteMethod(
         'upload',
@@ -19,7 +46,8 @@ module.exports = function(Postimage) {
                 {arg:'ctx',type:'object', http:{source: 'context'}},
                 {arg:'options', type:'object', http:{source:'query'}},
                 {arg:'access_token', type:'string'},
-                {arg: 'post_ID', type:'string'},
+                {arg: 'post_id', type:'string'},
+                {arg: 'user_id', type:'string'},
         ],
         returns:{
             arg: 'fileObject', type:'object', root:true,

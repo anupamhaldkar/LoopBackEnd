23c34a275d74d56cb19d0cb9725513210ce3fe4c
Anupam Haldkar <ahdev2020@outlook.com>
2021-01-20 23:07:15 +0530

    Revert "model relation 12:00"
    This reverts commit 65bfa4e30a9bbefd50b6ff12277ba818cfdb93ec.

--- server/server.js
+++ server/server.js
@@ -34,22 +34,7 @@ boot(app, __dirname, function(err) {
 });
 
 console.log(Object.keys(app.models));
-
-app.models.user.find((err,result) => {
-  if(result.length === 0){
-    const user = {
-      email:'a@haldkar.com',
-      password: 'test',
-      username: 'anu'
-    };
-    app.models.user.create(user,(err,result) =>{
-      console.log("Tried to create user",err,result);
-    });
-  }
-
-});
-
-app.models.user.afterRemote('create',  (ctx, user, next) => {
+app.models.User.afterRemote('create',  (ctx, user, next) => {
  console.log("New user is",user);
  app.models.Profile.create({
    first_name: user.username,
@@ -64,6 +49,5 @@ app.models.user.afterRemote('create',  (ctx, user, next) => {
    }
    next();
   });
-
 
 });
